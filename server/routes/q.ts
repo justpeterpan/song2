4a99a04b7021c421e70066cb7b47fc938236383d
justpeterpan <peter.busch@posteo.net>
2024-04-28 23:29:32 +0200

    docs: update readme with basic information

--- server/routes/q.ts
+++ server/routes/q.ts
@@ -9,7 +9,6 @@ export default defineEventHandler(() => {
     right: boolean
   }>
 
-  // Function to shuffle an array
   function shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1))
@@ -18,17 +17,15 @@ export default defineEventHandler(() => {
     return array
   }
 
-  // Pick randomly 4 albums from the list
   const selectedAlbums = shuffleArray(albums).slice(0, 4)
-  const rightIndex = Math.floor(Math.random() * selectedAlbums.length) // Determine the right album once
+  const rightIndex = Math.floor(Math.random() * selectedAlbums.length)
 
-  // Set 'right' to true for one album randomly and false for others
   const quiz: Quiz = selectedAlbums.map((album, index) => ({
     index: String(index),
     title: album.title,
     artist: album.artist,
     cover: album.cover,
-    right: index === rightIndex, // Randomly select one album to be right
+    right: index === rightIndex,
   }))
 
   return quiz
