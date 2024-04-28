40199d0480bf4f4320f786fc616ac49d61bfdb53
justpeterpan <peter.busch@posteo.net>
2024-04-28 23:22:37 +0200

    feat: improve button colors on wrong and right answers, mobile layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,52 @@
 <template>
-  <div class="mx-4 pt-4 text-white">
+  <div class="mx-4 pt-4 text-white h-screen grid place-content-center">
     <div
-      class="flex flex-col sm:flex-row justify-center w-full gap-8 sm:gap-24"
+      class="flex flex-col md:flex-row justify-center w-full gap-8 sm:gap-24"
     >
       <div
-        class="rounded-xl overflow-hidden shadow-2xl shadow-black/60 dark:border-gray-800 border-gray-600 border sm:min-w-[500px] sm:min-h-[500px] w-full lg:w-auto bg-gray-800"
+        class="rounded-xl overflow-hidden shadow-2xl shadow-black/60 dark:border-gray-800 border-gray-800 border sm:min-w-[500px] sm:min-h-[500px] w-full lg:w-auto bg-gray-800"
       >
         <img
+          v-if="quizAlbums?.length"
           :src="getRightAlbum(quizAlbums).cover"
           alt="album cover"
-          class="rounded-md shrink-0 w-full"
-          :class="{ 'blur-xl': !rightAnswered }"
+          class="rounded-md min-w-[320px] min-h-[320px] md:w-full md:h-full transition duration-300"
+          :class="[rightAnswered !== 'yet' ? 'blur-none' : 'blur-xl']"
         />
+        <div v-else class="w-[320px] h-[320px] rounded-md bg-neutral-800" />
       </div>
-      <div class="flex flex-col justify-center">
-        <div v-for="album in quizAlbums" :key="album.index" class="w-full mb-2">
-          <UButton
-            :color="
-              album.index === getRightAlbum(quizAlbums).index && rightAnswered
-                ? 'green'
-                : 'gray'
-            "
-            variant="solid"
-            class="w-full text-left"
-            size="xl"
-            @click="pickAlbum(album.index)"
+      <div
+        class="flex flex-col justify-center min-w-[320px] max-w-[320px] md:min-w-[500px] md:max-w-[500px]"
+      >
+        <template v-if="quizAlbums?.length">
+          <div
+            v-for="album in quizAlbums"
+            :key="album.index"
+            class="w-full mb-2"
           >
-            {{ album.artist }} - {{ album.title }}</UButton
-          >
-        </div>
+            <UButton
+              :color="
+                album.index === getRightAlbum(quizAlbums).index &&
+                (rightAnswered === 'yes' || rightAnswered === 'no')
+                  ? 'green'
+                  : album.index === pickedAlbumIndex && rightAnswered === 'no'
+                  ? 'red'
+                  : 'gray'
+              "
+              variant="solid"
+              class="w-full text-left"
+              size="xl"
+              @click="pickAlbum(album.index)"
+            >
+              {{ album.artist }} - {{ album.title }}</UButton
+            >
+          </div>
+        </template>
         <UButton
           icon="i-heroicons-arrow-path"
           variant="solid"
           color="violet"
-          class="mt-4 w-full mb-8 sm:mb-0"
+          class="mt-4 w-full mb-4 sm:mb-0"
           size="xl"
           block
           @click="reload"
@@ -54,7 +67,8 @@ type Album = {
   right: boolean
 }
 
-const rightAnswered = ref(false)
+const rightAnswered = ref<'yes' | 'no' | 'yet'>('yet')
+const pickedAlbumIndex = ref<string>('')
 
 const { data: quizAlbums } = await useFetch<Album[]>('/q')
 
@@ -73,14 +87,15 @@ function getRightAlbum(albums: Album[] | null): Album {
 
 function pickAlbum(index: string) {
   if (index === getRightAlbum(quizAlbums.value).index) {
-    rightAnswered.value = true
+    rightAnswered.value = 'yes'
   } else {
-    alert('Wrong!')
+    rightAnswered.value = 'no'
+    pickedAlbumIndex.value = index
   }
 }
 
 function reload() {
-  rightAnswered.value = false
+  rightAnswered.value = 'yet'
   quizAlbums.value = null
   fetch('/q')
     .then((res) => res.json())
