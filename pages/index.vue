46ca952c97afc32eae45129d38ab07b34bd0f704
justpeterpan <peter.busch@posteo.net>
2024-04-28 15:17:39 +0200

    feat: add refresh button

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="mx-4 pt-10 text-white">
-    <h1 class="text-3xl">Quiz</h1>
-    <div class="flex flex-col sm:flex-row justify-center w-full mt-20 gap-24">
+  <div class="mx-4 pt-4 text-white">
+    <div
+      class="flex flex-col sm:flex-row justify-center w-full gap-8 sm:gap-24"
+    >
       <div
         class="rounded-xl overflow-hidden shadow-2xl shadow-black/60 dark:border-gray-800 border-gray-600 border sm:min-w-[500px] sm:min-h-[500px] w-full lg:w-auto bg-gray-800"
       >
@@ -9,10 +10,10 @@
           :src="getRightAlbum(quizAlbums).cover"
           alt="album cover"
           class="rounded-md shrink-0 w-full"
-          :class="{ 'blur-2xl': !rightAnswered }"
+          :class="{ 'blur-xl': !rightAnswered }"
         />
       </div>
-      <div class="mt-8 sm:mt-0 flex flex-col justify-center">
+      <div class="flex flex-col justify-center">
         <div v-for="album in quizAlbums" :key="album.index" class="w-full mb-2">
           <UButton
             :color="
@@ -21,13 +22,23 @@
                 : 'gray'
             "
             variant="solid"
-            class="w-full"
+            class="w-full text-left"
             size="xl"
             @click="pickAlbum(album.index)"
           >
             {{ album.artist }} - {{ album.title }}</UButton
           >
         </div>
+        <UButton
+          icon="i-heroicons-arrow-path"
+          variant="solid"
+          color="violet"
+          class="mt-4 w-full mb-8 sm:mb-0"
+          size="xl"
+          block
+          @click="reload"
+          >Refresh</UButton
+        >
       </div>
     </div>
   </div>
@@ -66,5 +77,15 @@ function pickAlbum(index: string) {
   } else {
     alert('Wrong!')
   }
+}
+
+function reload() {
+  rightAnswered.value = false
+  quizAlbums.value = null
+  fetch('/q')
+    .then((res) => res.json())
+    .then((data) => {
+      quizAlbums.value = data
+    })
 }
 </script>
